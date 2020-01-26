772b2f87b039e80aa4ba951d5939e46115b11a95
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-26 20:54:15 +0000

    Automatically backed up by Learn

--- db/migrate/20200126205238_create_recipe.rb
+++ db/migrate/20200126205238_create_recipe.rb
@@ -3,6 +3,7 @@ class CreateRecipe < ActiveRecord::Migration
       create_table :recipes do |t|
       t.string :name 
       t.string :ingredients
-      t.string :
+      t.string :cook_time
+     end
   end
 end
